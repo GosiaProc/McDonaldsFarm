a11f92d7b83d300d1503c150d266e9af7f981b88
Gosia Proc <gosia.proc@gmail.com>
2022-04-22 12:26:54 +0200

    The program calculates max area,a,b for given x.

---
+++ McDonaldsFarm.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+  int x = 0 ;
+  int area = 0;
+  int areaMax = 0;
+  int aMax = 0;
+  int bMax = 0;
+  std::cout<<"HELP FOR UNCLE McDONALD\n";
+  std::cout<<"Enter the number of fence segments\n";
+  std::cin >> x;
+  if (x>0)
+  {
+    std::cout<<"Length of fence: ";
+    std::cout << x;
+    int a = 1;
+    while(a<=(x-2))
+      {
+        int b = x-(2*a);
+        int area=a*b;
+        if (area > areaMax)
+        {
+          areaMax=area;
+          aMax=a;
+          bMax=b;
+        } 
+        a++;
+      }
+   
+        std::cout<<"\nThe optimal area of McDonalds farm\n";
+        std::cout<< areaMax;
+        std::cout<<"\nThe optimal a lenght\n";
+        std::cout<< aMax;
+        std::cout<<"\nThe optimal b lenght\n";
+        std::cout<< bMax;
+    }
+  else
+  {
+    std::cout<<"Fence length must be positive integer\n";
+  }
+ 
+}
